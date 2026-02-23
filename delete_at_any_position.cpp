8ab0d5cc9a9b409c2327398ad526cfaab850725a
Mahir Mohtab Ayman <mahirmohtabayman@gmail.com>
2026-02-23 21:12:16 +0600

    code

---
+++ delete_at_any_position.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void delete_at_any_pos(Node *&head,int pos)
+{
+    Node *tmp = head;
+    for(int i=1;i<pos;i++)
+    {
+        tmp=tmp->next;
+    }
+    Node* deleteNode=tmp->next;
+    tmp->next=deleteNode->next;
+    deleteNode->next->prev=tmp;
+    delete deleteNode;
+}
+
+void forward(Node *&head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+void backward(Node *&tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *tail = new Node(40);
+
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+    b->next = tail;
+    tail->prev = b;
+    
+
+  //  forward(head);
+    delete_at_any_pos(head,1);
+    forward(head);
+
+    return 0;
+}
---
+++ delete_at_head.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node *prev;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+void delete_at_head(Node *&head, Node *&tail)
+{
+    Node *tmp = head;
+    head = head->next;
+    delete tmp;
+    if (head == NULL)
+    {
+        tail = NULL;
+        return;
+    }
+    head->prev = NULL;
+}
+
+void forward(Node *&head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+void backward(Node *&tail)
+{
+    Node *tmp = tail;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->prev;
+    }
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *tail = new Node(40);
+
+    head->next = a;
+    a->prev = head;
+    a->next = b;
+    b->prev = a;
+    b->next = tail;
+    tail->prev = b;
+    delete_at_head(head, tail);
+    delete_at_head(head, tail);
+
+    delete_at_head(head, tail);
+
+    forward(head);
+
+    return 0;
+}
